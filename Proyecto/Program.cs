d1dc14a95320a390b7a14ce0df01fef3028977da
4DownPortu <47735281+4DownPortu@users.noreply.github.com>
2019-12-04 17:05:20 -0800

    PROYECTO
    PROYEC

---
+++ Proyecto/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+namespace Proyecto
+{
+    class Producto
+    {
+        public string codigo 
+        {
+            get;
+            set;
+        }
+        public string descripcion
+        {
+            get;
+            set;
+        }
+        public double precio
+        {
+            get;
+            set;
+        }
+        public int departamento
+        {
+            get;
+            set;
+        }
+
+        public int likes
+        {
+            get;
+            set;
+        }
+
+
+        public Producto(string cod, string des, double pre, int depa, int lik)
+        {
+            codigo = cod;
+            descripcion = des;
+            precio= pre;
+            departamento =depa;
+            likes = lik;
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ creadir/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace creadir
+{
+    class Producto
+    {
+        public string codigo, descripcion;
+        public double precio;
+
+        public Producto (string c, string d, double p)
+        {
+            codigo=c;
+            descripcion=d;
+            precio=p;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Producto> productos = new List<Producto>();
+            productos.Add(new Producto("AQW","Lapiz Azul w2",12.23d));
+            productos.Add(new Producto("AQW","Lapiz Verde w2",23.13d));
+            productos.Add(new Producto("AQW","Pluma Negra w2",26.43d));
+            productos.Add(new Producto("AQW","Borrador w2",17.56d));
+
+            FileStream fs= new FileStream(@"productos.txt",FileMode.OpenOrCreate, FileAccess.Write);
+            StreamWriter txtOut = new StreamWriter(fs);
+
+            foreach(Producto p in productos)
+            {
+                txtOut.Write(p.descripcion + " ");
+                txtOut.WriteLine(p.precio);
+            }
+            fs.Close();
+        }
+    }
+}
